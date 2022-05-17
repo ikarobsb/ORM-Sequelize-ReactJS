5895030363e9d54d99fc6ec45797134168d2f9c0
ikarobsb <88170008+ikarobsb@users.noreply.github.com>
2022-05-17 13:32:34 -0300

    Primeiro commit

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: 'localhost',
+    dialect: 'mysql',
+    username: 'root',
+    password: '21111987Ipa',
+    database: 'webiiorm',
+    define: {
+        timestamps:true,
+        underscored: true,
+    },
+};
+
---
+++ frontend/src/components/AreaDados/AreaDados.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './AreaDados.css';
+import Jumbotrom from "../Jumbotrom/Jumbotrom";
+
+
+export default function AreaDados() {
+  return (
+    <div className="AreaDados">
+      <Jumbotrom />
+    </div>
+  );
+}
+
